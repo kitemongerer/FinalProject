9e1a16a90df0d24f88f86ce43805d8c50ba9e07d
Max Eshleman <meshlema@mines.edu>
2014-11-11 12:06:58 -0700

    Added reference files

--- src/FinalProject/Manager.java
+++ src/FinalProject/Manager.java
@@ -1,5 +1,8 @@
 package FinalProject;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Manager {
 
 	private Point[][] mine;
@@ -7,11 +10,14 @@ public class Manager {
 	public static final int NUM_ROBOTS = 4;
 	private static final int MINE_SIZE = 20;
 	private String inputFile;
+	private Queue<Robot> queue;
 	
 	public Manager(String inputFile) {
 		this.inputFile = inputFile;
 		readInputFile();
+		queue = new LinkedList<Robot>();
 		mine = new Point[MINE_SIZE][MINE_SIZE];
+		currentRobot = 0;
 	}
 	
 	public void sendRobot(int cavernNumber) {
