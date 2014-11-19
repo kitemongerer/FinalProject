7a0668929dd91e45d5a61dcdb5153a01ccada5c9
Corey Tokunaga-Reichert <ctokunag@mymail.mines.edu>
2014-11-18 18:34:50 -0700

    Caverns now a different color and each space on the path can be seen.

--- src/FinalProject/Mine.java
+++ src/FinalProject/Mine.java
@@ -24,8 +24,16 @@ public class Mine extends JPanel {
         g.setColor(Color.decode("#D2492A"));
         g.drawRect(0, 0, 20 * numRows, 20 * numCols);
         for (int row = 0; row < numRows; row++)
-            for (int col = 0; col < numCols; col++)
+            for (int col = 0; col < numCols; col++) {
+            	g.drawRect(col * 20, row * 20, 20, 20);
                 if (mine[row][col].type == PointType.WALL)
                     g.fillRect(col * 20, row * 20, 20, 20);
+            }
+                
+        g.setColor(Color.cyan);
+        for(int row = 0; row < numRows; row++)
+        	for(int col = 0; col < numCols; col++)
+        		if (mine[row][col].type == PointType.CAVERN)
+        			g.fillRect(col * 20, row * 20, 20, 20);
     }
 }
